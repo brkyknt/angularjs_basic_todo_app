641907decff8a13edaf09249f125a44c4fdda69b
Burak Aykanat <burak.aykanat12@gmail.com>
2017-09-15 21:11:23 +0300

    Add files via upload

---
+++ ToDoApp/app.js
@@ -0,0 +1,25 @@
+﻿var app = angular.module("ToDoApp", []);
+
+app.controller("TodoController", function ($scope) {
+
+    $scope.todos = [
+        { "ad": "Yapilacaklar 1", "isDone": false },
+        { "ad": "Yapilacaklar 2", "isDone": false },
+        { "ad": "Yapilacaklar 3", "isDone": false },
+        { "ad": "Yapilacaklar 4", "isDone": false }
+    ];
+
+    $scope.NewTodo = function(){
+        $scope.todos.push({
+            "ad": $scope.todo,
+            "isDone": false
+        });
+        $scope.todo = "";
+    }
+
+    $scope.DeleteTodos = function () {
+        $scope.todos = $scope.todos.filter(function (item) {
+            return !item.isDone;
+        });
+    }
+});
